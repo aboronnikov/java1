d9b419e3ef72da49996eec1287abccfaf86b60b5
redneckz <vasyaselsky@yandex.ru>
2024-02-25 14:31:04 +0300

    Refactor DAG factory methods

--- src/main/java/dagExample/DAGExample.java
+++ src/main/java/dagExample/DAGExample.java
@@ -1,19 +1,21 @@
 package dagExample;
 
-import java.util.*;
+import java.util.List;
 
 public class DAGExample {
     public static void main(String[] args) {
-        DAG<String> dag = new DAG<>(List.of("anton", "ilya", "pavel", "maksim"),
-                Map.of(0, Set.of(1, 2), 2, Set.of(3)));
+        var anton = new DAGItem<>("anton");
+        var ilya = new DAGItem<>("ilya");
+        var pavel = new DAGItem<>("pavel");
+        var maksim = new DAGItem<>("maksim");
 
-        System.out.println("Here we got edges:");
-        for (DAGEdge<String> edge : dag.getEdges()) {
-            System.out.println(edge.getItem1().getData() + ", " + edge.getItem2().getData());
-        }
+        var dag = new DAG<>(List.of(anton, ilya, pavel, maksim))
+                .addEdge(anton, ilya)
+                .addEdge(pavel, maksim)
+                .addEdge(maksim, anton);
+
+        System.out.println("Here we got items:");
+        dag.traverseItems((item) -> System.out.println("Item " + item.getData()));
     }
 
 }
-
-
-
